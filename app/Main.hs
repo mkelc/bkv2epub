8e94a8ffefe8bf16e1fbbd3d5a61a0b86e10e2b7
Marian Kelc <marian.kelc@eplus-gruppe.de>
2016-03-12 16:30:59 +0100

    Writing now section files, but not complete

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,16 @@ module Main where
 
 import Control.Arrow.ArrowIO
 import Control.Monad hiding (when)
+import Control.Monad.State hiding (when)
 import Text.XML.HXT.Core
 import Text.XML.HXT.Arrow.XmlState.RunIOStateArrow
 import Text.XML.HXT.Arrow.XmlState.TypeDefs
+import Data.Tree.NTree.TypeDefs
 import Data.AssocList
 import Text.Printf
 import Lib
+
+import qualified Text.XML.HXT.DOM.XmlNode as XN
 
 data Footnote = Footnote {
    ftid      :: Int,
@@ -27,56 +31,73 @@ data Pagebreak = Pagebreak {
 data Epubmeta = Epubmeta {
    idcnt     :: Int,
    seccnt    :: Int,
-   cfile     :: String,
    marks     :: [(String,Int)],
    footnotes :: [Footnote],
    pagebreaks:: [Pagebreak]
 }
 
 type EpubArrow b c = IOStateArrow Epubmeta b c
+type Epub = StateT Epubmeta IO
 
 bkvSource :: FilePath
 bkvSource = "D:/private/projects/cassian/"
 
-epubInit :: XIOState Epubmeta
-epubInit =  initialState $ Epubmeta 0 1 "" [] [] []
+epubInit :: Epubmeta
+epubInit =  Epubmeta 0 1  [] [] []
+
 
 main :: IO ()
 main = do
    fls <- filesFor "kapitel3050" 
    mapM putStrLn fls 
-   --forM fls bkvPar
-   bkvPar fls
+   (_,e) <- runEpub (eBkvPar fls) epubInit
    return ()
 
 filesFor :: String -> IO [FilePath]
 filesFor prefix = return $ takeWhile pureFileExist fls
    where fls = map ((++) bkvSource) $ series prefix
 
+eBkvPar :: [FilePath] -> Epub ()
+eBkvPar fs = do
+   n <- runEpubX (readAll fs >>> transform)
+   runEpubX outputFootnotes
+   runEpubX (constA (createSection n) >>> writeSection )
+   return () 
 
-bkvPar :: [FilePath] -> IO ()
-bkvPar f = do
-   (e,_) <- runEpub (epubInit) (readAll f >>> listA transform )
-   runXIOState (initialState $ e) (outputFootnotes)
-   return ()
+runEpub :: Epub a -> Epubmeta -> IO (a, Epubmeta)
+runEpub = runStateT
 
-runEpub :: XIOState Epubmeta -> IOStateArrow Epubmeta XmlTree c -> IO (Epubmeta,[c])
-runEpub s0 f = do
-   (finalState, res) <- runIOSLA (emptyRoot >>> f) s0 undefined
-   let usr = xioUserState finalState
-   return (usr,res)
-   where emptyRoot    = root [] []
+runEpubX :: IOStateArrow Epubmeta XmlTree c -> Epub [c]
+runEpubX f = do
+   e <- get
+   (x, res) <- lift $ runIOSLA (root [] [] >>> f) (initialState e) undefined
+   put $ xioUserState x
+   return res
 
+writeSection :: EpubArrow XmlTree XmlTree
+writeSection = 
+   getUserState &&& this >>> first (arr  inc >>> setUserState >>> arr wrt) >>> app
+   where wrt e = writeDocument [withIndent no, withOutputXHTML, withAddDefaultDTD yes] (printf "Section-%04d.xhtml" ((seccnt e)-1))
+         inc e = e {seccnt = 1 + (seccnt e)}
 
 readAll :: [String] -> EpubArrow b XmlTree
-readAll fls= constL fls >>> arr((++) "file://") >>> (setCurrentFile >>> readFromDocument [withValidate no, withParseHTML yes]) 
+readAll fls= constL fls >>> arr((++) "file://") >>> readFromDocument [withValidate no, withParseHTML yes] 
+
+collect :: EpubArrow [XmlTree] XmlTree
+collect = proc xs -> do
+   arr(\chlds -> NTree (XTag (mkName "html") []) chlds) -< xs
+
+final :: EpubArrow [XmlTree] XmlTree
+final = proc xs -> do
+   arr(\chlds -> NTree (XTag (mkName "merge") []) chlds) -< xs
+
+createSection :: [XmlTree] -> XmlTree
+createSection cs = XN.mkRoot [] [
+   XN.mkElement (mkName "html") [] [
+      XN.mkElement (mkName "body") [] cs ]]
 
 transform :: EpubArrow XmlTree XmlTree
-transform = proc x -> do
-   y <- multi (isContentTd `guards` filterBkvNode) -< x
-   str <- writeDocumentToString [withOutputEncoding "iso-8859-1", withIndent yes] -< y
-   arrIO (\s -> putStrLn s) -< str
-   returnA -< x
+transform = multi (isContentTd `guards` filterBkvNode) >>> getChildren
 
 isContentTd :: (ArrowXml a) => a XmlTree XmlTree
 isContentTd = isElem >>> hasName "td" >>> getChildren >>> isElem >>> hasName "h1"
@@ -129,7 +150,7 @@ outputFootnotes = proc x -> do
 
 dumpnotes :: Epubmeta -> IO ()
 dumpnotes e = do
-   forM (footnotes e) $ putStrLn . show
+   forM (reverse $ footnotes e) $ putStrLn . show
    return ()
       
 
@@ -150,12 +171,9 @@ nextId = proc x -> do
    setUserState -< e {idcnt = newid + 1}
    returnA -< newid
 
+nextSection :: Epubmeta -> Epubmeta
+nextSection e = e { seccnt= (seccnt e) + 1 }
+
 addMark :: EpubArrow (String,Int) (String,Int)
 addMark = arr id &&& getUserState >>> arr(\(s,e)-> (s, e { marks = s : (marks e) })) >>> second setUserState >>> arr fst
-
-setCurrentFile :: EpubArrow String String
-setCurrentFile = getUserState &&& (arr id) >>> arr(\(e,s)-> (e {cfile=s},s)) >>> first setUserState >>> arr snd
-
-getCurrentFile :: EpubArrow b String
-getCurrentFile = getUserState >>> arr(cfile)
 
